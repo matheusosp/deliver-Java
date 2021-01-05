eee5684963ef9d3be435c0b8f661ed322e2eaae7
matheusosp <matheusosp@users.noreply.github.com>
2021-01-05 19:56:21 -0300

    C:/Program Files/Git/order/{id}/delivered

--- src/main/java/com/deliver/deliver/controller/OrderController.java
+++ src/main/java/com/deliver/deliver/controller/OrderController.java
@@ -30,4 +30,9 @@ public class OrderController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+    @PatchMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
 }
